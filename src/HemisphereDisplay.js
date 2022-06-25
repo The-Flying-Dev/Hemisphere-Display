64a4caa0052f9f1da64522d59c0fb91a7f66b49e
work <leandranm@outlook.com>
2022-06-25 15:12:31 -0700

    Styles

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import './Hemisphere.css';
+import north from './images/north.jpg';
+import south from './images/south.jpg';
 
 //first argument passed to a functional component is always props
 //destructure props
 
 const HemisphereDisplay = ({ latitude }) => { 
-  const hemisphere = latitude > 0 ? 'You are in the Northern Hemisphere' : 'You are in the Southern Hemisphere';
+
+  const hemisphereConfig = {
+    Northern: {
+      text: 'You are in the Northern Hemisphere',
+      picture: north
+    },
+    Southern: {
+      text: 'You are in the Southern Hemisphere',
+      picture: south
+    }
+  }
+
+  const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
+  const { text, picture } = hemisphereConfig[hemisphere]
+
   return(
-    <div>{ hemisphere }</div>
+    <div className={hemisphere}>
+      <div className='ui raised text container segment'>
+        <div className='image'>
+          <img src={ picture } alt="world" />
+        </div>
+        <div className='ui teal bottom attached label'>
+          <h1>{ text } </h1>
+        </div>       
+      </div>    
+    </div>
   )
   
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import HemisphereDisplay from './HemisphereDisplay';
 
+
 class App extends React.Component {
 
 /*  
